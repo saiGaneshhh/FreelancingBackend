6ef5cc8fa71fc387819059ad56b5288b4d4516a5
Sai Ganesh Uppala <135828132+saiGaneshhh@users.noreply.github.com>
2026-03-07 14:33:15 +0530

    Update uploadSitiMasterController.js

--- Controllers/uploadSitiMasterController.js
+++ Controllers/uploadSitiMasterController.js
@@ -10,7 +10,7 @@ export const uploadSitiMasterController = async (req, res) => {
       });
     }
 
-    const workbook = XLSX.readFile(req.file.path);
+    const workbook = XLSX.read(req.file.buffer, { type: "buffer" });
     const sheetName = workbook.SheetNames[0];
 
     const sheetData = XLSX.utils.sheet_to_json(
