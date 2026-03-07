cb6f2a96d767f13196288a012682044754e7b655
Sai Ganesh Uppala <135828132+saiGaneshhh@users.noreply.github.com>
2026-03-07 14:34:47 +0530

    Update adminRoutes.js

--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -36,18 +36,11 @@ const router = express.Router();
 
 // ========== MULTER CONFIG ==========
 // ========== MULTER CONFIG (ALLOW ALL FILES) ==========
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 20 * 1024 * 1024 }, // 20MB limit
+  limits: { fileSize: 20 * 1024 * 1024 },
 });
 
 
